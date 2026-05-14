40a16b24f36b300ea0427ba904b8650adde2f313
Anton Waldron <1antonwaldron@gmail.com>
2026-05-14 23:43:12 +0100

    Add logistic regression analysis for staff turnover
    This script performs logistic regression analysis on staff turnover data, including data manipulation and model summary.

---
+++ LogisticRegTurnover.R
@@ -0,0 +1,52 @@
+# Logistic Regresssion Project
+set.seed(123)
+library(jtools)
+turnover <- read.csv('C:\\Users\\kingc\\OneDrive\\Desktop\\ADA\\Staff_Turnover.csv')
+
+head(turnover)
+
+
+
+# Data manipulation
+
+turnover$Attrition.f <- factor(turnover$Attrition,
+                                  levels = c(0,1),
+                                  labels = c('Stayed','Left'))
+turnover$BusinessTravel.f <- factor(turnover$BusinessTravel,
+                                       levels = c(2,3,4),
+                                       labels = c('Travel Frequently','Travel Rarely', 
+                                                 'No Travel'))
+
+turnover$Department.f <- factor(turnover$Department,
+                                   levels = c(1,2,3),
+                                   labels= c('HR', 'R&D', 'Sales'))
+
+turnover$Gender.f <- factor(turnover$Gender,
+                               levels = c(1,2),
+                               labels = c('Female', 'Male'))
+
+turnover$JobSatisfaction.f <- factor(turnover$JobSatisfaction,
+                                        levels = c(0,1,2),
+                                        labels = c('Low', 'Medium', 'High'))
+
+#Make an 80% training set and 20% testing set
+
+#sample <- sample(c(TRUE,FALSE), nrow(turnover), replace=TRUE, prob=c(0.8,0.2))
+#train <- turnover[sample,]
+#test <- turnover[!sample,]
+
+glm_model <- glm(Attrition.f ~ BusinessTravel.f + Department.f + Gender.f +
+                 + JobSatisfaction.f + EmployeeID + HourlyRate + DistanceFromHome +
+                 + Age, data=turnover, family = binomial)                              
+
+summary(glm_model)
+                                       
+#significant are Business Travel, Job Satisfaction, Age, Distance from home
+
+# Exponentiate coefficients to get Adjusted Odds Ratios
+exp(coef(glm_model))
+# Obtain confidence intervals and exponentiate
+exp(confint(glm_model))
+
+summ(glm_model)
+                                    
